994ca8134f5c8db3613421a5350cbe3b4653ec5e
Gustavo Costa <gustavocosta.cba@gmail.com>
2024-08-10 15:14:49 -0400

    Dapper

--- src/Endpoints/Employees/EmployeeGetAll.cs
+++ src/Endpoints/Employees/EmployeeGetAll.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Identity;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 namespace IWantApp.Endpoints.Employees;
 
@@ -7,17 +8,18 @@ public class EmployeeGetAll
     public static string Template => "/employees";
     public static string[] Methods => new string[] {HttpMethod.Get.ToString()};
     public static Delegate Handle => Action;
-    public static IResult Action(UserManager<IdentityUser> userManager)
+    public static IResult Action(int page, int rows, IConfiguration configuration)
     {
-        var users = userManager.Users.ToList();
-        var employees = new List<EmployeeResponse>();
-        foreach(var item in users)
-        {
-            var claims = userManager.GetClaimsAsync(item).Result;
-            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-            var userName = claimName != null? claimName.Value : string.Empty;
-            employees.Add(new EmployeeResponse(item.Email, userName));
-        }
+        var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+        var query = @"select Email, ClaimValue as Name
+                from AspNetUsers u inner 
+                join AspNetUsersClaims c
+                on u.id = c.UserId and claimtype = 'Name'
+                offset (@page -1) * @rows ROWS FETCH NEXT @rows ROWS ONLY";
+        var employees = db.Query<EmployeeResponse>(
+            query,
+            new {page,rows}
+        );
         return Results.Ok(employees);
     }
 }
